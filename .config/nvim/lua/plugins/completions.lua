582eea3437910931bd2b45b67f4c6462d54a505d
José Miguel Sarasola <alosarjos@gmail.com>
2024-02-05 07:25:55 +0100

    nvim: DO not use completion value if not explicitely

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -30,13 +30,14 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "buffer" },
 					{ name = "crates" },
+          { name = "codeium" }
 				}),
 			})
 		end,
