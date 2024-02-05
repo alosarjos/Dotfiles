6a200937544e2232983fc9fd22ff0c58341fd9fa
José Miguel Sarasola <alosarjos@gmail.com>
2024-02-05 07:26:41 +0100

    nvim: Initialize toggleterm as any other plugin

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,1 +1,7 @@
-return { "akinsho/toggleterm.nvim", version = "*", config = true }
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+    require("toggleterm").setup()
+	end,
+}
