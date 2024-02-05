92a613a3551254785c58564bdb12c790b07867eb
José Miguel Sarasola <alosarjos@gmail.com>
2024-02-05 07:27:13 +0100

    nvim: Add treesitter-textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 return {
+  {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
@@ -10,4 +15,13 @@ return {
 			indent = { enable = true },
 		})
 	end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = true,
+    config = function ()
+      require("nvim-treesitter.configs").setup({
+      })
+    end
+  }
 }
